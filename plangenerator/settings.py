a95d81d55f47ff7ad2d471f6a553d882c5bc323c
Vishnu V <vishnuvaivid@gmail.com>
2024-01-29 19:27:58 +0530

    Update settings.py

--- plangenerator/settings.py
+++ plangenerator/settings.py
@@ -20,15 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jzb%s0!$(^yxb)))mj6d-6bbjhx@h7u%-or_p5!#jt)gvwfflh'
+
+
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Frue
 
 ALLOWED_HOSTS = []
 
